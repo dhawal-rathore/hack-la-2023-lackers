4c6a7110b0994ebfa7cbca5eefa200e4db83db68
Manan Daga <manandaga2004@gmail.com>
2023-11-04 16:49:47 -0700

    Update main.py with cleaner outputs

--- BackEnd/main.py
+++ BackEnd/main.py
@@ -14,57 +14,46 @@ for course in courses:
     try:
         course.name
         curr_courses.append(course)
-        print(course)
     except:
         pass
 
 stat_assignments = curr_courses[-1].get_assignments()
 
-for assignment in stat_assignments:
-    # convert string due_at to datetime object and check if it is past due
-
-    if assignment.attributes['name'] == "Midterm 1 - 2023W1":
-        print(assignment.attributes)
+for course in curr_courses:
+    print(f"Course Name: {course}")
+    print()
+    print(f"Assignments:")
     
-    if assignment.attributes['due_at'] is None:
-        print(f"{assignment.attributes['name']} has no due date with max points: {assignment.attributes['points_possible']}")
+    if (len(list(course.get_assignments())) == 0):
+        print("No assignments")
+        print()
+        continue
+    for assignment in course.get_assignments():
+        if assignment.attributes['due_at'] is None:
+            print(f"{assignment.attributes['name']} has no due date with max points: {assignment.attributes['points_possible']}")
+            continue
+
+        assignment.attributes['due_at'] = datetime.datetime.strptime(assignment.attributes['due_at'], '%Y-%m-%dT%H:%M:%SZ')
+        if assignment.attributes['due_at'] > datetime.datetime.now():
+            print(f"{assignment.attributes['name']} is due on {assignment.attributes['due_at']} with max points: {assignment.attributes['points_possible']}")
+        else:
+            print(f"{assignment.attributes['name']} was due on {assignment.attributes['due_at']} with max points: {assignment.attributes['points_possible']}")
+
+    print()
+
+    print("Announcements:")
+    announcements = canvas.get_announcements(context_codes=[f"course_{course.id}"])
+
+    if (len(list(announcements)) == 0):
+        print("No announcements")
+        print()
         continue
 
-    assignment.attributes['due_at'] = datetime.datetime.strptime(assignment.attributes['due_at'], '%Y-%m-%dT%H:%M:%SZ')
-    if assignment.attributes['due_at'] > datetime.datetime.now():
-        print(f"{assignment.attributes['name']} is due on {assignment.attributes['due_at']} with max points: {assignment.attributes['points_possible']}")
-    else:
-        print(f"{assignment.attributes['name']} was due on {assignment.attributes['due_at']} with max points: {assignment.attributes['points_possible']}")
+    for a in announcements:
+        html_text = BeautifulSoup(a.attributes['message'], 'html.parser')
+        p_tags = html_text.find_all('p')
+        message = ' '.join([p.get_text() for p in p_tags])
+        print(f"{a.attributes['title']}:\n{message}")
 
-print()
-print()
-print()
-print()
-print()
-
-announcements = canvas.get_announcements(context_codes=[f"course_{curr_courses[4].id}"]) 
-print(len(list(announcements)))
-
-
-for a in announcements:
-    html_text = BeautifulSoup(a.attributes['message'], 'html.parser')
-    p_tags = html_text.find_all('p')
-    message = ' '.join([p.get_text() for p in p_tags])
-    print(f"{a.attributes['title']}:\n{message}")
     print()
-    print()
-
-
-
-# print(midterm)
-
-# print(len(list(curr_courses_quizzes[126181])))
-# stat_quizes = []
-
-# for quiz in curr_courses_quizzes[126181]:
-#     stat_quizes.append(quiz)
-
-# for quiz in stat_quizes:
-#     print(quiz.attributes)
-
 
